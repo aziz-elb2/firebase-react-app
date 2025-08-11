375f3b247d008bd677950a0a17a8d4423fcf8efb
Aziz El Abbassi <azizelabbassi5@gmail.com>
2025-08-11 07:35:29 +0100

    Added Daisy ui & first Style Applied

--- src/HomeBook.jsx
+++ src/HomeBook.jsx
@@ -15,6 +15,14 @@ import {
 
 import { useNavigate } from "react-router-dom";
 
+import {
+  ExclamationTriangleIcon,
+  EyeIcon,
+  MagnifyingGlassIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+
 const HomeBook = () => {
   const [books, setBooks] = useState([]);
   const [isloading, setIsloading] = useState(true);
@@ -72,79 +80,129 @@ const HomeBook = () => {
 
   const handleDelete = (id) => {
     const docRef = doc(db, "books", id);
-    deleteDoc(docRef).then((snapshot) => {
-      console.log("Book Deleted Succefuly");
+    deleteDoc(docRef).then(() => {
+      return (
+        <div className="toast">
+          <div className="alert alert-info">
+            <span>Book Deleted Succefuly</span>
+          </div>
+        </div>
+      );
     });
   };
 
   return (
-    <>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-2 space-y-2">
+      {/* <div className="toast toast-top z-1000">
+        <div className="alert alert-error text-white p-2 ">
+          <span className="font-thin flex items-center justify-between gap-2">
+            <ExclamationTriangleIcon className="h-4 " /> Book Deleted
+            Succefuly
+          </span>
+        </div>
+      </div> */}
 
+      <div className="card w-full bg-base-100 card-sm shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title font-thin justify-center ">Add Book</h2>
+          <form onSubmit={(e) => handleSubmit(e)} className="space-y-2">
+            <input
+              type="text"
+              placeholder="Title"
+              value={newBook.title}
+              className="input"
+              onChange={(e) =>
+                setNewBook((old) => ({ ...old, title: e.target.value }))
+              }
+            />
+            <input
+              type="text"
+              placeholder="Author"
+              value={newBook.author}
+              className="input"
+              onChange={(e) =>
+                setNewBook((old) => ({ ...old, author: e.target.value }))
+              }
+            />
+            <input
+              type="number"
+              value={newBook.pages}
+              placeholder="Pages"
+              className="input validator"
+              onChange={(e) =>
+                setNewBook((old) => ({
+                  ...old,
+                  pages: parseInt(e.target.value),
+                }))
+              }
+            />
 
+            <button className="btn btn-soft btn-info font-thin  btn-wide max-w-full hover:text-white  ">
+              Add Book
+            </button>
+          </form>
+        </div>
+      </div>
 
-      <form className="p-4 mb-4 " onSubmit={(e) => handleSubmit(e)}>
-        <h4>Add Book Form</h4>
-        <input
-          type="text"
-          placeholder="Title"
-          value={newBook.title}
-          onChange={(e) =>
-            setNewBook((old) => ({ ...old, title: e.target.value }))
-          }
-        />
-        <input
-          type="text"
-          placeholder="Author"
-          value={newBook.author}
-          onChange={(e) =>
-            setNewBook((old) => ({ ...old, author: e.target.value }))
-          }
-        />
-        <input
-          type="number"
-          value={newBook.pages}
-          placeholder="Pages"
-          onChange={(e) =>
-            setNewBook((old) => ({ ...old, pages: parseInt(e.target.value) }))
-          }
-        />
-        <button className="bg-blue-600 border-blue-800 rounded-sm text-white p-1">
-          Add Book
-        </button>
-      </form>
+      <div className="card w-full bg-base-100 card-sm shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title font-thin">Books Pages</h2>
 
-      <form>
-        <h4>Filter books by pages bigger than</h4>
-        <input
-          type="number"
-          className="border-1 "
-          onChange={(e) => setFilterPages(parseInt(e.target.value))}
-        />
-      </form>
+          <label className="input">
+            <MagnifyingGlassIcon className="h-[1em] opacity-50" />
+            <input
+              type="number"
+              onChange={(e) => setFilterPages(parseInt(e.target.value))}
+            />
+          </label>
+        </div>
+      </div>
 
-      <h4>Display Books</h4>
       {isloading && <h3>Loading ... </h3>}
       {error && <h3>Error : {error}</h3>}
-      {books &&
-        books.map((book) => (
-          <div key={book.id}>
-            <h4
-              className="text-blue-700 underline font-bold cursor-pointer"
-              onClick={() => navigate(`/viewBook/${book.id}`)}
-            >
-              {book.title}
-            </h4>
-            <p>{book.author}</p>
-            <span>{book.pages}</span>
-            <button
-              onClick={() => handleDelete(book.id)}
-              className="bg-red-500 p-1 cursor-pointer text-amber-50"
-            >
-              Delete Book
-            </button>
-          </div>
-        ))}
-    </>
+
+      <ul className="list bg-base-100 rounded-box shadow-md  ">
+        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
+          The best books
+        </li>
+
+        {books &&
+          books.map((book) => (
+            <li className="list-row">
+              <div>
+                <img
+                  className="size-10 rounded-box"
+                  src="https://leaders.com/wp-content/uploads/2023/02/Database-bio-profile-pic-28.png"
+                />
+              </div>
+              <div>
+                <Link
+                  to={`/viewBook/${book.id}`}
+                  className="hover:text-blue-600"
+                >
+                  <div>{book.author}</div>
+                </Link>
+                <div className="text-xs uppercase font-semibold opacity-60">
+                  {book.title}
+                </div>
+              </div>
+              <div className="flex justify-end items-center">
+                <Link to={`/viewBook/${book.id}`}>
+                  <button className="btn btn-square btn-ghost hover:text-blue-500">
+                    <EyeIcon className="size-[1.2em]" />
+                  </button>
+                </Link>
+                <button
+                  className="btn btn-square btn-ghost hover:text-red-500"
+                  onClick={() => handleDelete(book.id)}
+                >
+                  <TrashIcon className="size-[1.2em]" />
+                </button>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 };
 
--- src/ViewBook.jsx
+++ src/ViewBook.jsx
@@ -1,9 +1,16 @@
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { db } from "./firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  NumberedListIcon,
+  XMarkIcon,
+  BookOpenIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
 const ViewBook = () => {
+  const navigate = useNavigate();
   const [loading, setloading] = useState(true);
   const [error, setError] = useState(null);
   const [book, setBook] = useState({});
@@ -13,16 +20,15 @@ const ViewBook = () => {
   const [updatedBook, setUpdatedBook] = useState({});
 
   useEffect(() => {
-    setloading(true);
     getDoc(docRef)
       .then((doc) => {
         const newbook = {
           id: doc.id,
           ...doc.data(),
         };
+        setloading(false);
         setBook(newbook);
         setUpdatedBook(newbook);
-        setloading(false);
       })
       .catch((error) => {
         // console.log(error);
@@ -40,52 +46,81 @@ const ViewBook = () => {
   };
 
   return (
-    <>
-      <Link to="/">
-        <button>Back</button>
-      </Link>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-2 space-y-2">
       {error && <p>{error}</p>}
-      {loading && <p>Loading ...</p>}
-      {book && (
-        <form className="flex flex-col" onSubmit={(e) => handleUpdate(e)}>
-          
-          <input
-            type="text"
-            value={updatedBook.title}
-            onChange={(e) =>
-              setUpdatedBook((old) => ({
-                ...old,
-                title: e.target.value,
-              }))
-            }
-          />
-          <input
-            type="text"
-            value={updatedBook.author}
-            onChange={(e) =>
-              setUpdatedBook((old) => ({
-                ...old,
-                author: e.target.value,
-              }))
-            }
-          />
-          <input
-            type="number"
-            min={0}
-            max={10000}
-            value={updatedBook.pages}
-            onChange={(e) =>
-              setUpdatedBook((old) => ({
-                ...old,
-                pages: parseInt(e.target.value),
-              }))
-            }
-          />
 
-          <button className="bg-blue-500  text-white">Modify</button>
-        </form>
-      )}
-    </>
+      <div className="card w-full bg-base-100 card-sm shadow-sm">
+        <div className="card-body">
+          <div className="card-title  justify-between items-center font-thin">
+            <span></span>
+            <span>View Book</span>
+            <XMarkIcon
+              onClick={() => navigate("/")}
+              className="h-[1em] hover:text-red-500 cursor-pointer transform transition-transform  ease-out duration-200    hover:scale-110"
+            />
+          </div>
+          {loading ? (
+            <div className="space-y-1">
+              <div className="skeleton h-4 w-full p-5"></div>
+              <div className="skeleton h-4 w-full p-5"></div>
+              <div className="skeleton h-4 w-full p-5"></div>
+            </div>
+          ) : (
+            <form className="space-y-1" onSubmit={(e) => handleUpdate(e)}>
+              <label className="input">
+                <BookOpenIcon className="h-[1em] opacity-50" />
+                <input
+                  type="text"
+                  value={updatedBook.title}
+                  onChange={(e) =>
+                    setUpdatedBook((old) => ({
+                      ...old,
+                      title: e.target.value,
+                    }))
+                  }
+                />
+              </label>
+              <label className="input">
+                <UserIcon className="h-[1em] opacity-50" />
+
+                <input
+                  type="text"
+                  value={updatedBook.author}
+                  onChange={(e) =>
+                    setUpdatedBook((old) => ({
+                      ...old,
+                      author: e.target.value,
+                    }))
+                  }
+                />
+              </label>
+              <label className="input">
+                <NumberedListIcon className="h-[1em] opacity-50" />
+
+                <input
+                  type="number"
+                  min={0}
+                  max={10000}
+                  value={updatedBook.pages}
+                  onChange={(e) =>
+                    setUpdatedBook((old) => ({
+                      ...old,
+                      pages: parseInt(e.target.value),
+                    }))
+                  }
+                />
+              </label>
+
+              <div className="justify-end card-actions">
+                <button className="btn btn-soft btn-secondary btn-wide max-w-full ">
+                  Modify
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
